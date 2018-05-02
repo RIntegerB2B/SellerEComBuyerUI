34a50dbf24b7f3ad41dd55259560c8bc58c399ff
Chandran <Preveen_Chandran@Dell.com>
2018-05-02 19:32:21 +0530

    Config Changes

--- src/app/config/appSetting.ts
+++ src/app/config/appSetting.ts
@@ -1,5 +1,5 @@
 export const AppSetting : AppSettingType = {
     awsServiceUrl:"http://ec2-13-127-165-9.ap-south-1.compute.amazonaws.com:3010/",
     local3010BuyerServiceUrl:"http://localhost:3010/",
-    serviceUrl:"http://ec2-13-127-137-140.ap-south-1.compute.amazonaws.com:3010/"
+    serviceUrl:"http://ec2-13-127-198-141.ap-south-1.compute.amazonaws.com:3010/"
 }
